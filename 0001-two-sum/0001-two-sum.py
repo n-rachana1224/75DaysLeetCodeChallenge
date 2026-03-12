43764d27940a8c624cf0e4b1ac9ea2d370a650c1
n-rachana1224 <nrachana101@gmail.com>
2026-03-12 10:45:46 +0530

    Time: 0 ms (100%), Space: 12.8 MB (98.17%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i,num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num],i]
+            seen[num] = i
+        return [-1,-1]
+
+       
