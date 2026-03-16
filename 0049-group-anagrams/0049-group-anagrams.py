e281ecefdcc55da24ffb519766071ec098e7ad2b
n-rachana1224 <nrachana101@gmail.com>
2026-03-16 10:46:35 +0530

    Time: 23 ms (53.36%), Space: 16 MB (79.84%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def groupAnagrams(self, strs):        
+        res=defaultdict(list)
+        for s in strs:
+            key=''.join(sorted(s))
+            res[key].append(s)
+        return list(res.values())
+
