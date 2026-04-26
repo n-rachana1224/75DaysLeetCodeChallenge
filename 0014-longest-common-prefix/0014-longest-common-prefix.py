5882a5eba5c97aa6eda5f4600fab1367cf821aa6
n-rachana1224 <nrachana101@gmail.com>
2026-04-26 15:00:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (71.01%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        strs.sort()
+        first, last = strs[0], strs[-1]
+        
+        i = 0
+        while i < len(first) and first[i] == last[i]:
+            i += 1
+        
+        return first[:i]
+
+
+        
