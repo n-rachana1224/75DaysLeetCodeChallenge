1ac842881521def4ea152494aa97645b9741f6e3
n-rachana1224 <nrachana101@gmail.com>
2026-05-01 20:03:39 +0530

    Time: 33 ms (74.65%), Space: 14.7 MB (72.13%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        curr_sum = 0
+        prefix_sum = {0: 1}  # Important initialization
+        
+        for num in nums:
+            curr_sum += num
+            
+            if curr_sum - k in prefix_sum:
+                count += prefix_sum[curr_sum - k]
+            
+            prefix_sum[curr_sum] = prefix_sum.get(curr_sum, 0) + 1
+        
+        return count
